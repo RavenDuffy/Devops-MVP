ffe90dd448cbd7cf006b7615b08ce1d470b321b8
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2022-02-13 16:34:22 +0000

    separated cmds

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,19 @@
-import express from "express"
-import cors from "cors"
+import express from 'express';
+import cors from 'cors';
 
-const PORT = 4005
+const PORT = 4005;
 
-const server = express()
-server.use(express.json())
-server.use(cors({ origin: true, credentials: true }))
-server.use(express.static(__dirname))
-server.set("trust_proxy", 1)
+const server = express();
+server.use(express.json());
+server.use(cors({ origin: true, credentials: true }));
+server.use(express.static(__dirname));
+server.set('trust_proxy', 1);
 
-server.get("/", (req, res) => {
+server.get('/', (req, res) => {
   res.json({
     Welcome: req.ip,
-  })
-})
+    Message: 'hello',
+  });
+});
 
-server.listen(PORT)
+server.listen(PORT);
